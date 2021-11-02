a2e707e5c08ea1aba5d3bc159e2d9c90a040839c
ilia <piryazevilia@gmail.com>
2021-11-02 14:45:08 +0600

    add scents to index

--- js/main.js
+++ js/main.js
@@ -130,35 +130,6 @@ $(window).on('load', function() {
     navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
   });
 
-
-  /*------------------
-    Features Slider
-  --------------------*/
-  $('.features-slider').owlCarousel({
-    nav: true,
-    dots:false,
-    margin: 30,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-    responsive : {
-      0 : {
-        items: 1,
-      },
-      576 : {
-        items: 2,
-
-      },
-      768 : {
-        items: 3,
-      },
-      991 : {
-        items: 4,
-      },
-      1200 : {
-        items: 5,
-      }
-    }
-  });
-
   /*------------------
     About Slider
   --------------------*/
@@ -170,36 +141,6 @@ $(window).on('load', function() {
     items: 1,
     navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
   });
-
-  /*------------------------------
-    Property Features Slider
-  -------------------------------*/
-  $('.property-features-slider').owlCarousel({
-    nav: true,
-    dots:false,
-    items: 3,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-    margin: 30,
-    responsive : {
-      0 : {
-        items: 1,
-        margin: 20,
-      },
-      576 : {
-        items: 2,
-        margin: 20,
-      },
-      768 : {
-        items: 3,
-        margin: 20,
-      },
-      991 : {
-        items: 3,
-        margin: 30,
-      }
-    }
-  });
-
 
   /*------------------
     Video Popup
